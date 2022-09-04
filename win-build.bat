99f9aec37e668f322673655d38c025d8e67fcec5
meteyu <metecyu@gmail.com>
2022-09-04 16:11:10 +0800

    去掉 win32 相关内容

--- win-build.bat
+++ win-build.bat
@@ -22,17 +22,10 @@ set GOOS=windows
 set GOARCH=amd64
 go build --tags fts5 -v -o "../app/kernel/SiYuan-Kernel.exe" -ldflags "-s -w -H=windowsgui" .
 
-set GOOS=windows
-set GOARCH=386
-go build --tags fts5 -v -o "../app/kernel32/SiYuan-Kernel.exe" -ldflags "-s -w -H=windowsgui" .
-cd ..
 
 echo 'Building Electron'
 cd app
 call pnpm run dist
-echo 'Building Electron win32'
-call pnpm run dist-win32
-cd ..
 
 echo 'Building Appx'
 electron-windows-store --input-directory app\build\win-unpacked --output-directory app\build\ --package-version 1.0.0.0 --package-name SiYuan --manifest app\appx\AppxManifest.xml --assets app\appx\assets\ --make-pri true
